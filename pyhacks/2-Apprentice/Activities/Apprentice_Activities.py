745715b953305cc59bf9d36910d9da251a9894f4
jiotaro <monkeydjiotaro@gmail.com>
2017-03-30 09:41:04 +0700

    back to clean

--- pyhacks/2-Apprentice/Activities/Apprentice_Activities.py
+++ pyhacks/2-Apprentice/Activities/Apprentice_Activities.py
@@ -1,19 +1,15 @@
 def activity01(num1):
 	'''Determine if an input number is Even or Odd'''
-	return 'Even' if num1 % 2 == 0 else 'Odd'
+	return
 		
 def activity02(iv_one, iv_two):
 	'''Return the sum of two input values'''
-	return iv_one + iv_two
+	return
 
 def activity03(num_list):
 	'''Given a list of integers, count how many are even'''
-	count = 0
-	for n in num_list:
-		if n % 2 == 0: count += 1
-	return count
+	return
 	
 def activity04(input_string):
 	'''Return the input string, backward'''
-	return input_string[::-1]
-	
+	return
